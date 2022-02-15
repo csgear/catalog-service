8a1facfc404e266e8c8e353280938150e3f39554
Xiaojun Yang <csgear@live.com>
2022-02-15 13:47:11 -0500

    Setup

---
+++ src/main/java/com/vkeonline/bookstore/catalogservice/config/PolarProperties.java
@@ -0,0 +1,10 @@
+package com.vkeonline.bookstore.catalogservice.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "polar")
+public record PolarProperties(
+
+		String greeting
+
+){}
